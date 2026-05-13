64e2c244cfceb7d197171b97191eacd4a58f69ff
Alejandro Villaseñor <alefeco2003@gmail.com>
2026-05-12 18:49:21 -0600

    [updated] se agregaron visuales y se mejoro la logica del checkIn

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,9 +1,10 @@
-// FUNCIÓN : Para usuarios con cuenta ()
+// FUNCIÓN para usuarios con cuenta (staff o estudiante)
 async function iniciarSesion() {
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const message = document.getElementById('message');
 
+    // Validación de campos vacíos
     if (!email || !password) {
         message.innerText = "😨 Por favor llena todos los campos";
         return;
@@ -21,7 +22,7 @@ async function iniciarSesion() {
         if (response.ok) {
             localStorage.setItem('user', JSON.stringify(data.user));
             
-            // Redirección inteligente basada en el ROL que viene de la DB
+            // Redirección según rol
             if (data.user.role === 'staff') {
                 window.location.href = 'dashboard-staff.html';
             } else {
@@ -35,49 +36,16 @@ async function iniciarSesion() {
     }
 }
 
-// FUNCIÓN 2: Para el botón de "Seguir sin cuenta"
-
+// FUNCIÓN para el botón de "Comprar sin iniciar sesión"
 function entrarComoInvitado() {
-    // 1. Creamos un objeto que simule ser un usuario de la base de datos
+    // Crear objeto usuario invitado
     const invitado = { 
         name: "Invitado", 
         role: "student", 
         isGuest: true 
     };
-
-    // 2. Lo guardamos en el localStorage para que el Dashboard sepa quién entró
+    // Guardar en localStorage
     localStorage.setItem('user', JSON.stringify(invitado));
-
-    // 3. Redirigimos al archivo correspondiente
+    // Redirigir al dashboard de estudiante
     window.location.href = 'dashboard-estudiante.html';
-}
-
-async function iniciarSesion() {
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-
-    try {
-        const response = await fetch('http://localhost:3000/api/auth/login', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ email, password })
-        });
-
-        const data = await response.json();
-
-        if (response.ok) {
-            localStorage.setItem('user', JSON.stringify(data.user));
-
-            // REDIRECCIÓN AUTOMÁTICA 
-            if (data.user.role === 'staff') {
-                window.location.href = 'dashboard-staff.html';
-            } else {
-                window.location.href = 'dashboard-estudiante.html';
-            }
-        } else {
-            document.getElementById('message').innerText = "❌ " + data.message;
-        }
-    } catch (error) {
-        document.getElementById('message').innerText = "❌ Error de conexión.";
-    }
 }
